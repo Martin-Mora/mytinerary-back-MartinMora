97e08e062ce72619a9fcd0676c649c8ad7a95d20
Martin Mora <martin_mora1209@hotmail.com>
2026-02-06 20:07:33 -0300

    fix: server.listen con fallback para Render

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 import 'dotenv/config.js';
-import  Express from 'express';
+import Express from 'express';
 import indexRouter from './router/indexRouter.js';
 import cors from 'cors';
-import './config/database.js'
-import notFoundHandler from './middlewares/notFoundHandler.js'
+import './config/database.js';
+import notFoundHandler from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
-
 
 const server = Express();
 
-server.use(cors())
+server.use(cors());
 server.use(Express.json());
 server.use('/api', indexRouter);
 
 server.use(notFoundHandler);
 server.use(errorHandler);
 
+// 🚀 SERVER LISTEN CORREGIDO
+const PORT = process.env.PORT || 3000;
 
-server.listen(process.env['PORT'], ()=> { console.log('Corriendo en el server' + process.env['PORT'])})
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
+
 
